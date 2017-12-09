aacd3724852f788484f541dc16421a40603336f1
crazdrms <dilipmaharjann@gmail.com>
2017-12-08 20:50:30 -0500

    function and dynamic array

--- src/hello.kt
+++ src/hello.kt
@@ -1,3 +1,11 @@
 fun main(args:Array<String>){
     println("Hello Kotlin")
+    printArray()
+}
+
+fun printArray(){
+    var array= arrayOf(1,2,3,"Hello","hi","what")
+    for (a in array){
+        println(a)
+    }
 }
