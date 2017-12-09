1583811e5c72d7eef5719243a0d5502c4b939677
crazdrms <dilipmaharjann@gmail.com>
2017-12-08 21:04:21 -0500

    range and reverse funcitons

--- src/hello.kt
+++ src/hello.kt
@@ -1,11 +1,21 @@
-fun main(args:Array<String>){
+fun main(args: Array<String>) {
     println("Hello Kotlin")
     printArray()
 }
 
-fun printArray(){
-    var array= arrayOf(1,2,3,"Hello","hi","what")
-    for (a in array){
+fun printArray() {
+    var array = arrayOf(1, 2, 3, "Hello", "hi", "what")
+    for (a in array) {
         println(a)
     }
+
+    var range = 1..5
+    for (i in range) {
+        println(i)
+    }
+    var reverse=5 downTo 1
+    for (i in reverse.reversed()){
+        println(i)
+    }
 }
+
